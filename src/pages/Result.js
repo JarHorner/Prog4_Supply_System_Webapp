fa1b8d4bd7c8f5209a58eeb27dda57024c03690a
Jarret Horner <55000163+JarHorner@users.noreply.github.com>
2022-12-02 17:39:52 -0700

    Even more base functionality. waiting for database. (#13)

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,11 +1,29 @@
 
 
+import resultClasses from "../css/Result.module.css";
+
 function Result() {
 
     return (
-        <>
+        <div className={resultClasses.resultPage}>
             <h1>Results</h1>
-        </>
+            <div className={resultClasses.informationRow}>
+                <h3>ID:</h3>
+                <p className={resultClasses.information}>item ID</p>
+            </div>
+            <div className={resultClasses.informationRow}>
+                <h3>Quantity:</h3>
+                <p className={resultClasses.information}>item quantity</p>
+            </div>
+            <div className={resultClasses.informationRow}>
+                <h3>Price:</h3>
+                <p className={resultClasses.information}>item price</p>
+            </div>
+            <div className={resultClasses.informationRow}>
+                <h3>Supplier ID:</h3>
+                <p className={resultClasses.information}>item supplier ID</p>
+            </div>
+        </div>
     );
 }
 
