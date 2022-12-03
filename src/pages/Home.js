e3cbc9c0ca6e66cdf17a5a9f0b0ebf0a263027a2
Jarret Horner <jhorn384@mtroyal.ca>
2022-12-02 17:37:55 -0700

    Even more base functionality. waiting for database.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,27 @@
 
 
+import homeClasses from "../css/Home.module.css";
+
 function Home() {
 
     return (
-        <>
+        <div className={homeClasses.homePage}>
             <h1>Home</h1>
             <input 
                 type="text"
                 placeholder="Enter name of item">
             </input>
-            <button>Enter</button>
-            <div>
+            <button className={homeClasses.enterButton}>Enter</button>
+            <div className={homeClasses.itemsList}>
                 {/*This is where the list of items go!*/}
+                <div>item name 1</div>
+                <div>item name 2</div>
+                <div>item name 3</div>
+                <div>item name 4</div>
+                <div>item name 5</div>
+                <div>item name 6</div>
             </div>
-        </>
+        </div>
     );
 }
 
